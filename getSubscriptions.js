9563fc265b6941e8f1dcbcd31bf8fed05672db41
archibald <archibald@favorlabs.io>
2022-09-20 15:01:25 +0800

    fix:  add before block config

--- getSubscriptions.js
+++ getSubscriptions.js
@@ -32,7 +32,7 @@ class processor extends events{
             if(dbNumber<this.number){
                 throw new Error("get dbNumber error");
             }
-            let toBlock = this.number+this.stage-before;
+            let toBlock = this.number + this.stage - 1;
             if(dbNumber < toBlock){
                 toBlock = dbNumber;
             }
@@ -120,7 +120,9 @@ class processor extends events{
         }
         catch (e){
             console.error(`some error: ${e}`.red);
-            this.emit('start');
+            setTimeout(()=>{
+                this.emit('start')
+            },500)
         }
     }
 
