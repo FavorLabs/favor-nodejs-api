4339cffa6840e2ee3c7ccfa40a1ebdfe137504cf
archibald <archibald@favorlabs.io>
2022-09-26 17:29:03 +0800

    feat:  add invitations

--- routes/shares.js
+++ routes/shares.js
@@ -10,12 +10,24 @@ const { protect } = require('../middleware/auth')
 
 
 router.route('/:id').get(async(req, res, next)=>{
-  let v = await Video.findById(req.params.id)
-  v.query = new URLSearchParams(req.query).toString()
-  if(v.query){
-    v.query = "?"+v.query;
+  if(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i.test(req.params.id)){
+    let v = await Video.findById(req.params.id)
+    v.query = new URLSearchParams(req.query).toString()
+    if(v.query){
+      v.query = "?"+v.query;
+    }
+    res.render('video', v);
   }
-  res.render('share', v);
+  else {
+
+    res.render('share', {url:req.url});
+  }
+
 })
+
+// router.route('/').get(async(req, res, next)=>{
+//
+//   res.render('share', {});
+// })
 
 module.exports = router
