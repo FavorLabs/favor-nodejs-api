4f15bfe17edc0aac23a9f1b7bf5c1eab6627f322
ChesterFL <Chester@favorlabs.io>
2022-10-21 17:38:55 +0800

    feat: get user by code

--- controllers/users.js
+++ controllers/users.js
@@ -6,62 +6,69 @@ const User = require('../models/User')
 // @route   GET /api/v1/auth/users
 // @access  Private/Admin
 exports.getUsers = asyncHandler(async (req, res, next) => {
-  res.status(200).json(res.advancedResults)
+    res.status(200).json(res.advancedResults)
 })
 
 // @desc    Get single user
 // @route   GET /api/v1/auth/users/:id
 // @access  Private/Admin
 exports.getUser = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id).populate({
-    path: 'subscribers'
-  }).populate({ path: 'invitations' });
+    let user;
+    const checkForHexRegExp = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i
+    if (checkForHexRegExp.test(req.params.id)) {
+        user = await User.findById(req.params.id).populate({
+            path: 'subscribers'
+        }).populate({path: 'invitations'});
+    } else {
+        user = await User.findOne({code: req.params.id})
+    }
+    if (!user)
+        return next(new ErrorResponse(`No user with that id of ${req.params.id}`))
 
-  if (!user)
-    return next(new ErrorResponse(`No user with that id of ${req.params.id}`))
-
-  res.status(200).json({ success: true, data: user })
+    res.status(200).json({success: true, data: user})
 })
 
 // @desc    Create user
 // @route   POST /api/v1/auth/users
 // @access  Private/Admin
 exports.createUser = asyncHandler(async (req, res, next) => {
-  req.body.channelName = (req.body.channelName || "").trim();
-  const user = await User.create(req.body)
+    req.body.channelName = (req.body.channelName || "").trim();
+    const user = await User.create(req.body)
 
-  res.status(201).json({ success: true, data: user })
+    res.status(201).json({success: true, data: user})
 })
 
 // @desc    Update user
 // @route   PUT /api/v1/auth/users/:id
 // @access  Private/Admin
 exports.updateUser = asyncHandler(async (req, res, next) => {
-  req.body.password = ''
-  delete req.body.password
-  req.body.channelName = (req.body.channelName || "").trim();
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    context: 'query'
-  })
+    req.body.password = ''
+    delete req.body.password
+    req.body.channelName = (req.body.channelName || "").trim();
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        context: 'query'
+    })
 
-  if (!user)
-    return next(new ErrorResponse(`No user with that id of ${req.params.id}`))
+    if (!user)
+        return next(new ErrorResponse(`No user with that id of ${req.params.id}`))
 
-  res.status(200).json({ success: true, data: user })
+    res.status(200).json({success: true, data: user})
 })
 
 // @desc    Delete user
 // @route   DELETE /api/v1/auth/users/:id
 // @access  Private/Admin
 exports.deleteUser = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id)
+    const user = await User.findById(req.params.id)
 
-  if (!user)
-    return next(new ErrorResponse(`No user with that id of ${req.params.id}`))
+    if (!user)
+        return next(new ErrorResponse(`No user with that id of ${req.params.id}`))
 
-  await User.findByIdAndDelete(req.params.id)
+    await User.findByIdAndDelete(req.params.id)
 
-  res.status(200).json({ success: true, data: {} })
+    res.status(200).json({success: true, data: {}})
 })
+
+
